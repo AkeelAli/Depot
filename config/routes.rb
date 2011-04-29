f470cfddf59569da1220e0a391894496ac6abda2
Akeel <agilowen@gmail.com>
2011-04-29 01:41:20 -0400

    Store is now at root. CSS added and Layout adjusted

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Depot::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
   # The priority is based upon order of creation:
@@ -52,6 +54,7 @@ Depot::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
 
+  root :to => 'store#index', :as => 'store'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
